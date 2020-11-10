707cd94d0bbded926fa73379f3ab976635912100
Chris Ting <christht@usc.edu>
2020-11-10 01:36:58 -0800

    big hat changes

---
+++ Assets/Scripts/Experiment/WinterHat.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WinterHat : Hat
+{
+    public WinterHat(GameObject visual)
+        : base(HatBehavior.HatType.Winter, visual)
+    {
+
+    }
+
+
+    public override void Activate(PlayerMovement player)
+    {
+        base.Activate(player);
+
+        // longer attack time
+        //faster push time
+        if (player.pushTime >= 0.5f)
+        {
+            player.bar.GetComponent<Image>().color = Color.blue;
+            player.pushTime /= 2f;
+            Debug.Log($"Winter Hat Push Time: {player.pushTime}");
+        }
+    }
+    public override void Attach(PlayerMovement player)
+    {
+
+    }
+
+    public override void Detach(PlayerMovement player)
+    {
+        player.bar.GetComponent<Image>().color = Color.red;
+        player.pushTime *= 2f;
+    }
+}
